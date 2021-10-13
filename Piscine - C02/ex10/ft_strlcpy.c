44d136bab7767debbd358569d0ad2315fcc9c011
EderTolentino <68605527+EderTolentino@users.noreply.github.com>
2021-10-13 14:04:47 +0100

    Piscine - C02

---
+++ Piscine - C02/ex10/ft_strlcpy.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eluiz-to <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/12 19:08:20 by eluiz-to          #+#    #+#             */
+/*   Updated: 2021/08/13 14:58:31 by eluiz-to         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	c;
+	unsigned int	i;
+
+	c = 0;
+	i = 0;
+	while (src[c] != '\0')
+	{
+		c++;
+	}
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (c);
+}
+
+#include <stdio.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size);
+
+int		main(void)
+{
+	char src[] = "Source";
+	char dest[] = "";
+	unsigned int n;
+
+	n = 4;
+
+	printf("BEFORE\n\tsrc: %s\n\tdes: %s\n\tsize: %d\n", src, dest, n);
+
+    n = ft_strlcpy(dest, src, n);
+
+	printf("AFTER\n\tsrc: %s\n\tdes: %s\n\tsize: %d", src, dest, n);
+	return (0);
+}
